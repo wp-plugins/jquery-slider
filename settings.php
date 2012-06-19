c5a37bb0e2c21cec0cb201ae9fe8dbee64b3b907
wp-contents <wp-contents@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-19 10:11:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jquery-slider/tags/1.3@560404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ settings.php
@@ -0,0 +1,63 @@
+<?php
+
+// Adding options page
+function js_menu() {
+	add_options_page('jQuery Slider','jQuery Slider','manage_options','js_options','js_options');
+}
+add_action('admin_menu', 'js_menu');
+
+function js_options(){
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	?>
+	<form action="options.php" method="post">
+	  <div class="wrap">
+		<?php wp_nonce_field('update-options') ?>
+		  <h2>jQuery Slider <?php _e('Settings', 'jquery_slider') ?></h2>
+		  <table border="0" cellspacing="6" cellpadding="6">
+			<tr>
+			  <td><?php _e('Width', 'jquery_slider') ?></td>
+			  <td><input name="js_width" type="text" id="js_width" value="<?php echo get_option('js_width'); ?>" size="4" />px</td>
+			</tr>
+			<tr>
+			  <td><?php _e('Height', 'jquery_slider') ?></td>
+			  <td><input name="js_height" type="text" id="js_height" value="<?php echo get_option('js_height'); ?>" size="4" />px</td>
+			</tr>
+			<tr>
+			  <td><?php _e('Pause on hover', 'jquery_slider') ?></td>
+			  <td>
+			  <select name="js_pause" id="js_pause">
+				<option value="true" <?php if(get_option('js_pause')) echo "selected" ?>><?php _e('Yes', 'jquery_slider') ?></option>
+				<option value="false" <?php if(!get_option('js_pause')) echo "selected" ?>><?php _e('No', 'jquery_slider') ?></option>
+			  </select></td>
+			</tr>
+			<tr>
+			  <td><?php _e('Show pagination', 'jquery_slider') ?></td>
+			  <td>
+			  <select name="js_paging" id="js_paging">
+				<option value="true" <?php if(get_option('js_pause')) echo "selected" ?>><?php _e('Yes', 'jquery_slider') ?></option>
+				<option value="false" <?php if(!get_option('js_pause')) echo "selected" ?>><?php _e('No', 'jquery_slider') ?></option>
+			  </select></td>
+			</tr>
+			<tr>
+			  <td><?php _e('Show navigation', 'jquery_slider') ?></td>
+			  <td>
+			  <select name="js_nav" id="js_nav">
+				<option value="true" <?php if(get_option('js_pause')) echo "selected" ?>><?php _e('Yes', 'jquery_slider') ?></option>
+				<option value="false" <?php if(!get_option('js_pause')) echo "selected" ?>><?php _e('No', 'jquery_slider') ?></option>
+			  </select></td>
+			</tr>
+			<tr>
+			  <td><span class="submit">
+			  <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="page_options" value="js_width,js_height,js_pause,js_paging,js_nav" />
+				<input type="submit" class="button-primary" value="<?php _e('Save Changes', 'jquery_slider') ?>" />
+			  </span></td>
+			  <td>&nbsp;</td>
+			</tr>
+		  </table>
+		</div>
+	</form>
+	<?php
+}
